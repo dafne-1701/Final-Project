ddc6c047ed1ed9b0d9394e0946850be1d1039819
hei <161633312+heitml@users.noreply.github.com>
2026-05-05 12:51:19 -0700

    Update Lighting_System.h; Added Studio_Mode function

--- Lighting_System.h
+++ Lighting_System.h
@@ -1,5 +1,13 @@
 /**
 * @file Lighting_System.h
 * 
-* @brief Header file for .
+* @brief Header file for Lighting System driver
 */
+
+#include "TM4C123GH6PM.h"
+#include "GPIO.h"
+
+/**
+* 
+**/
+void Lighting_Studio_Mode(void);

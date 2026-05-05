7e9ac573507df0f73df47e33a8f40aca772003bc
hei <161633312+heitml@users.noreply.github.com>
2026-05-05 12:34:15 -0700

    Update GPIO.c; RGB & White LED pins assigned

--- GPIO.c
+++ GPIO.c
@@ -7,10 +7,10 @@
 // PORT B: For LED connections for our Studio/Party Modes
 // PB0: Studio White Lights
 // PB1: Party RED Light
-// PB2: Party YELLOW Light
+// PB2: Party GREEN Light
 // PB3: Party BLUE light
-// PB4: Party GREEN light
-// PB5-PB0: 0x1F
+// PB3-0: 0x0F
+
 
 // PE3: Used for 
 #include "GPIO.h"
@@ -19,10 +19,10 @@
 void GPIO_PortB_Init(void)
 {
   SYSCTL -> RCGCGPIO |= 0x02; // Port B clock on
-  GPIOB->DIR   |=  0x1F; // Output Pins
-  GPIOB->AFSEL &= ~0x1F; // GPIO function
-  GPIOB->AMSEL &= ~0x1F;
-  GPIOB->DEN   |=  0x1F; // Digital enable
-  GPIOB->DATA  &= ~0x1F; // Intially off
+  GPIOB->DIR   |=  0x0F; // Output Pins
+  GPIOB->AFSEL &= ~0x0F; // GPIO function
+  GPIOB->AMSEL &= ~0x0F;
+  GPIOB->DEN   |=  0x0F; // Digital enable
+  GPIOB->DATA  &= ~0x0F; // Intially off
 
 }

ae45a7411e8b09541b466e7ded9729b734ef8fea
hei <161633312+heitml@users.noreply.github.com>
2026-05-05 12:47:51 -0700

    Update Lighting_System.c; Added Studio_Mode function

--- Lighting_System.c
+++ Lighting_System.c
@@ -1,5 +1,22 @@
 /**
 * @file Lighting_System.c
 * 
-* @brief Source file for .
+* @brief Source file for Lighting System driver
+*
+* Pins Used for Lights:
+*   PB0 - White LED lights
+*   PB1 - RGB Red LED
+*   PB2 = PGB Green LED
+*   PB3 - RGB Blue LED
 */
+
+#include "Lighting_System.h"
+
+void Lighting_Studio_mode(void)
+{
+  // RGB turned off in case it is on beforehand
+  GPIOB -> DATA &= ~0x0E;
+
+  // Turn on White LED lights
+  GPIOB -> DATA |= 0x01;
+}

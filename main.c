78e313488dcbe1b866c75f35ec48f37e37b5b825
hei <161633312+heitml@users.noreply.github.com>
2026-05-09 22:53:38 -0700

    Update main.c; Loop for Lighting Modes Included

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include "GPIO.h"
 #include "Lighting_System.h"
 #include "Timing.h"
+#include "SysTick_Delay.h"
 
 #define LIGHT_BUTTON 0x04
 #define PHOTO_BUTTON 0x08
@@ -22,10 +23,36 @@ int main(void)
 GPIO_PortB_Init();
 GPIO_PortE_Init();
 Lighting_Off();
+SysTick_Delay_Init();
 
 while(1)
 {
-  // Lighting Button is pressed
-  if ((GPIOE -> DATA & LIGHT_BUTTON) == 0)
+  if ((GPIOE -> DATA & LIGHT_BUTTON) == 0) // Button is Pressed
 {
-  
+  SysTick_Delay1ms(20);
+while ((GPIOE -> DATA & LIGHT_BUTTON) == 0);
+
+mode++; // Cycles forward to OFF, STUDIO, PARTY
+
+if (mode > 2)
+{
+mode = 0;   // Default is set as Lighting OFF
+}
+
+if (mode == 0)
+{
+  Lighting_Off(); // 0 = LIGHTING OFF when not pressed
+}
+    else if (mode == 1)
+    {
+    Lighting_Studio_Mode(); // When Pressed, Studio Mode is turned on
+  }
+}
+
+if (mode == 2)
+{
+  Lighting_Party_Mode();
+SysTick_Delay1ms(300);
+    }
+  }
+}

ce91f4332f459c5244153b12167d5badb1a2d71a
hei <161633312+heitml@users.noreply.github.com>
2026-05-09 21:07:28 -0700

    Update Lighting_System.h

--- Lighting_System.h
+++ Lighting_System.h
@@ -8,7 +8,22 @@
 #include "GPIO.h"
 #include <stdint.h>
 
+#define WHITE 0x01
+#define RED 0x02
+#define GREEN 0x04
+#define BLUE 0x08
+
 /**
-* 
+* @brief Used to turn all the LEDs off before going into Party or Studio mode. 
+**/
+void Lighting_Off(void);
+
+/**
+* @brief Used to activate steady white LEDs for a more "Professional" / Calm Mode
 **/
 void Lighting_Studio_Mode(void);
+
+/**
+* @brief Used to activate a sequence of RED, GREEN, BLUE Leds for a "Fun" Mode
+**/
+void Lighting_Party_Mode(void);
